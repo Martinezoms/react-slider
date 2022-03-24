4c8f4afb3f724e760fff9a1a7176bf39efa2e53a
Zoms <martinezoms15@gmail.com>
2022-03-24 18:05:44 +0100

    added slider arrows

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
   const [width, setWidth] = useState(0);
 
   const carousel = useRef();
+  const wrapper = useRef();
+
+  let index = 0;
+
+  const moveSlider = (direction) => {
+    if (direction === "right") {
+      if (carousel.current.offsetWidth * index >= carousel.current.scrollWidth) {
+        index = 0;
+      } else {
+        index++;
+      }
+    }
+    if (direction === "left") {
+      if (index === 0) {
+        index = 0;
+      } else {
+        index--;
+      }
+    }
+    wrapper.current.style.transform = `translateX(-${carousel.current.offsetWidth * index}px)`;
+  };
 
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
@@ -18,7 +39,12 @@ function App() {
         <span>Zoms Design</span> <span>React Slider</span>
       </nav>
       <motion.div ref={carousel} className="carousel">
+        <div className="slider-arrows">
+          <div onClick={() => moveSlider("left")}>{"<"}</div>
+          <div onClick={() => moveSlider("right")}>{">"}</div>
+        </div>
         <motion.div
+          ref={wrapper}
           whileTap={{ cursor: "grabbing" }}
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
